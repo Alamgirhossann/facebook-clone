052be066e4ba44966f900d9e03454dfa0c7ebd44
Alamgirhossann <alamgirhossann@gmail.com>
2023-04-04 15:37:52 +0600

    fix url

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,7 +5,7 @@ export const authOptions = {
   providers: [
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      clientSecret: process.env.NEXTAUTH_SECRET,
     }),
     // ...add more providers here
   ],
